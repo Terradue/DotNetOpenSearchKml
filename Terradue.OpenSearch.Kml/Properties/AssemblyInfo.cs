390538a7d8e177a12374e9fd405eecf2c685166d
Emmanuel Mathot <emmanuel.mathot@skynet.be>
2014-06-24 17:40:48 +0200

    new OpenSearch

--- Terradue.OpenSearch.Kml/Properties/AssemblyInfo.cs
+++ Terradue.OpenSearch.Kml/Properties/AssemblyInfo.cs
@@ -1,30 +1,49 @@
-/*                                                            
-* Copyright (C) 2010-2014 Terradue S.r.l.
-* 
-* This file is part of Terradue.OpenSearch.
-* 
-* Foobar is free software: you can redistribute it and/or modify it under the terms of the 
-* GNU General Public License as published by the Free Software Foundation, 
-* either version 3 of the License, or (at your option) any later version.
-* 
-* Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-* See the GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License along with Terradue.OpenSearch.
-* If not, see http://www.gnu.org/licenses/.
-*/
-
+﻿//
+//  AssemblyInfo.cs
+//
+//  Author:
+//       Emmanuel Mathot <emmanuel.mathot@terradue.com>
+//
+//  Copyright (c) 2014 Terradue
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
 using System.Reflection;
 using System.Runtime.CompilerServices;
-[assembly: AssemblyTitle ("Terradue.OpenSearch")]
-[assembly: AssemblyDescription ("Terradue .Net OpenSearch Library")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Terradue")]
-[assembly: AssemblyProduct ("Terradue.OpenSearch")]
-[assembly: AssemblyCopyright ("Terradue")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
-[assembly: AssemblyVersion ("1.5.0.*")]
-[assembly: AssemblyInformationalVersion ("1.5.0.0")]
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Terradue.OpenSearch.Kml")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Terradue")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Terradue")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
 
