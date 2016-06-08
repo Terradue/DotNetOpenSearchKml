00aa0cc1b3e49242dfe4579b16ad664bead694be
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-06-08 10:42:14 +0200

    ready to pack 1.2
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.OpenSearch.Kml/Properties/AssemblyInfo.cs
+++ Terradue.OpenSearch.Kml/Properties/AssemblyInfo.cs
@@ -38,8 +38,8 @@ using NuGet4Mono.Extensions;
 [assembly: AssemblyAuthors("Emmanuel Mathot")]
 [assembly: AssemblyProjectUrl("https://github.com/Terradue/DotNetOpenSearchKml")]
 [assembly: AssemblyLicenseUrl("https://github.com/Terradue/DotNetOpenSearchKml/blob/master/LICENSE")]
-[assembly: AssemblyVersion("1.1.4.*")]
-[assembly: AssemblyInformationalVersion("1.1.4")]
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyInformationalVersion("1.2")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
